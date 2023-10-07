8f2285b4b1a98e5c3a0b6cb5cd16a3541af7d84f
Ivan Enzel <enzel@live.com>
2023-10-07 13:37:13 +0500

    added exit in  tests

--- test.sh
+++ test.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
-pip install -r requirements-test.txt
+echo "--- Running tests ---"
 
-if python -m unittest discover -s tests/ | grep OK ;
-    then echo "Tests runned"
+if python -m unittest discover -s tests/ | grep OK
+then 
+    echo "--- Tests runned ---"
+else
+    echo "Test did not ran OK"
+    exit 1
 fi
 
+echo "--- Generating report ---"
 
-if coverage run -m unittest discover -s tests/ | grep OK ;
-    then echo "App covered with tests"
+if coverage run -m unittest discover -s tests/ | grep OK
+then 
+    echo "App covered with tests"
+else
+    echo "Report did not generated"
+    exit 1
 fi
 
 coverage report --include crawler/crawler.py
