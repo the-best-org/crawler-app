cb705aacca0aa3c83a55fbb59ae4dc3a3e6423a5
Ivan Enzel <enzel@live.com>
2023-10-07 13:04:48 +0500

    Fix env for tests

--- test.sh
+++ test.sh
@@ -3,12 +3,12 @@
 pip install -r requirements-test.txt
 
 if python -m unittest discover -s tests/ | grep OK ;
-    echo "Tests runned"
+    then echo "Tests runned"
 fi
 
 
 if coverage run -m unittest discover -s tests/ | grep OK ;
-    echo "App covered with tests"
+    then echo "App covered with tests"
 fi
 
 coverage report --include crawler/crawler.py
